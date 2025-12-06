52a8438f8d5bde45149e18fd5af88e8714cfa1f6
GAURAV SONI <14952642+gauravsonii@users.noreply.github.com>
2025-12-06 23:22:12 +0530

    Fix: Aggressively suppress SyntaxWarning from tweepy library

--- app.py
+++ app.py
@@ -1,5 +1,41 @@
 import os
-import tweepy
+import sys
+
+# Set environment variable to suppress warnings at Python level (before any imports)
+os.environ['PYTHONWARNINGS'] = 'ignore::SyntaxWarning'
+
+import warnings
+
+# Aggressively suppress SyntaxWarning from tweepy library BEFORE any imports
+# This must be done at the very start to catch warnings during module import
+# Python 3.12+ treats these warnings as errors in some environments like Vercel
+
+# Set default action to ignore SyntaxWarnings
+warnings.simplefilter('ignore', SyntaxWarning)
+
+# Filter all SyntaxWarnings
+warnings.filterwarnings('ignore', category=SyntaxWarning)
+warnings.filterwarnings('ignore', message='.*invalid escape sequence.*')
+warnings.filterwarnings('ignore', message='.*invalid.*escape.*')
+
+# Patch warnings.showwarning to completely suppress SyntaxWarnings
+_original_showwarning = warnings.showwarning
+
+def _suppress_syntax_warnings(message, category, filename, lineno, file=None, line=None):
+    # Only show warnings that are NOT SyntaxWarning
+    if not issubclass(category, SyntaxWarning):
+        try:
+            _original_showwarning(message, category, filename, lineno, file, line)
+        except Exception:
+            pass  # Ignore any errors in warning display
+
+warnings.showwarning = _suppress_syntax_warnings
+
+# Suppress warnings during tweepy import
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore', SyntaxWarning)
+    warnings.filterwarnings('ignore', category=SyntaxWarning)
+    import tweepy
 import config
 from auth import require_bearer_token
 from flask import Flask, request, jsonify, render_template, send_from_directory
